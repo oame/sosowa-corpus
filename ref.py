745e5de0caf6ac8a8d4bbd60485209827cebe180
o_ame <oame@oameya.com>
2013-06-14 11:45:43 +0900

    bug fixed

--- ref.py
+++ ref.py
@@ -5,16 +5,17 @@ import nltk
 from nltk.corpus.reader import *
 from nltk.corpus.reader.util import *
 from nltk.text import Text
-import MeCab
+#import MeCab
 import pprint
 
 jp_sent_tokenizer = nltk.RegexpTokenizer(u'[^　「」！？。]*[！？。]')
 jp_chartype_tokenizer = nltk.RegexpTokenizer(u'([ぁ-んー]+|[ァ-ンー]+|[\u4e00-\u9FFF]+|[^ぁ-んァ-ンー\u4e00-\u9FFF]+)')
-ss = PlaintextCorpusReader(".", r'data/sosowa-all.txt',
+ss = PlaintextCorpusReader(".", r'data/novels/sosowa-all-plain.txt',
                           encoding='utf-8',
                           para_block_reader=read_line_block,
                           sent_tokenizer=jp_sent_tokenizer,
                           word_tokenizer=jp_chartype_tokenizer)
+
 text = Text(w.encode("utf-8") for w in ss.words())
 fdist = nltk.FreqDist(ss.words())
 fdist.plot(50, cumulative=True)
